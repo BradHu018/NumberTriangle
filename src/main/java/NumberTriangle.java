eb897b43d9962218ff8af731fe6c45c85217cf6e
Nubcoder0118 <brad.hu.2006@gmail.com>
2025-09-25 20:53:44 -0400

    implement retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -90,8 +90,19 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
+        if (path.isEmpty()) {
+            return root;
+        }
+        if (isLeaf()) {
+            return root;
+        }
+        else if (path.charAt(0) == 'r') {
+            return right.retrieve(path.substring(1));
+        }
+        else if (path.charAt(0) == 'l') {
+            return left.retrieve(path.substring(1));
+        }
         return -1;
-
     }
 
     /** Read in the NumberTriangle structure from a file.
